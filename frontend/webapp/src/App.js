4344c52e87c7055da52d0cc168670035b8e5a1b9
Tinna Liu <tinnaliu@berkeley.edu>
2021-09-18 11:04:47 -0700

    router

--- frontend/webapp/src/App.js
+++ frontend/webapp/src/App.js
@@ -1,13 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import ButtonAppBar from './Components/ButtonAppBar';
+import Navigation from './Components/Navigation';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <ButtonAppBar />
+        <ButtonAppBar/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
