afc63085ba540169f299e963537c416a4bdcf111
Jerry Pan <jerrypan2718@berkeley.edu>
2021-09-18 12:00:08 -0700

    calendar import fixed

--- frontend/webapp/src/Components/Calendar.js
+++ frontend/webapp/src/Components/Calendar.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@mui/material/Typography';
+import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@mui/material/';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@material-ui/icons/Save';
-import CalendarPickerSkeleton from '@mui/lab/CalendarPickerSkeleton';
+// import CalendarPickerSkeleton from '@mui/lab/CalendarPickerSkeleton';
 // or
-// import { CalendarPickerSkeleton } from '@mui/lab';
+import { CalendarPickerSkeleton } from '@mui/lab';
 
-const App = () => {
+const Calendar = () => {
     return (
         <>
             <CssBaseline />
@@ -23,3 +21,5 @@ const App = () => {
         </>
     );
 }
+
+export default Calendar;
