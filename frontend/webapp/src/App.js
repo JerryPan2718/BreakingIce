7df8775bbf81abb6a9940bc43ae0c48467189bce
Tinna Liu <tinnaliu@berkeley.edu>
2021-09-18 14:32:42 -0700

    fixed navbar, redirect to explore

--- frontend/webapp/src/App.js
+++ frontend/webapp/src/App.js
@@ -1,39 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 
-import Container from '@mui/material/Container';
-import Box from '@mui/material/Box';
+import Container from "@mui/material/Container";
 
-import ButtonAppBar from './Components/ButtonAppBar';
+import ButtonAppBar from "./Components/ButtonAppBar";
 
-import AddGame from './pages/AddGame';
-import Explore from './pages/Explore';
-import Profile from './pages/Profile';
-import Calendar from 'react-calendar';
+import AddGame from "./pages/AddGame";
+import Explore from "./pages/Explore";
+import Profile from "./pages/Profile";
 function App() {
   return (
-        <Router>
-          <div>
-            <ButtonAppBar/>
-          </div>
-          <div>
-            <Container maxWidth="sm" class="App-body">
-                <Switch>
-                  <Route exactly component={Explore} path="/" />
-                  <Route exactly component={Explore} path="/explore" />
-                  <Route exactly component={AddGame} path="/add-game" />
-                  <Route exactly component={Profile} path="/profile" />
-                </Switch>
-            </Container>
-          </div>
-        </Router>
+    <Router>
+      <ButtonAppBar/>
+        <Container maxWidth="sm" class="App-body">
+            <Switch>
+              {/* <Route exactly component={Explore} path="/" /> */}
+              <Route exactly component={Explore} path="/explore" />
+              <Route exactly component={AddGame} path="/add-game" />
+              <Route exactly component={Profile} path="/profile" />
+              <Redirect to="/explore" />
+            </Switch>
+        </Container>
+    </Router>
   );
 }
 
