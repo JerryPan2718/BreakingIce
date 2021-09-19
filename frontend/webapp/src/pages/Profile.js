e409e298518161efbd3a1438ae886a89b5731b35
Tinna Liu <tinnaliu@berkeley.edu>
2021-09-18 23:06:17 -0700

    temp

--- frontend/webapp/src/pages/Profile.js
+++ frontend/webapp/src/pages/Profile.js
@@ -84,34 +84,4 @@ function Profile() {
     );
 };
 
-
-// const styles = StyleSheet.create({
-//     navBar: {
-//         height: 60,
-//         flexDirection: 'row',
-//         justifyContent: 'space-between',
-//         alignItems: 'center',
-//         backgroundColor: 'blue',
-//     },
-//     leftContainer: {
-//         flex: 1,
-//         flexDirection: 'row',
-//         justifyContent: 'flex-start',
-//         backgroundColor: 'green'
-//     },
-//     rightContainer: {
-//         flex: 1,
-//         flexDirection: 'row',
-//         justifyContent: 'flex-end',
-//         alignItems: 'center',
-//         backgroundColor: 'red',
-//     },
-//     rightIcon: {
-//         height: 10,
-//         width: 10,
-//         resizeMode: 'contain',
-//         backgroundColor: 'white',
-//     }
-// });
-
 export default Profile;
