8a612693eac761c4e62e46d66f350e7547766d6f
AnimatorJoe <josephjin7@gmail.com>
2021-09-18 23:05:42 -0700

    merging screen components

--- frontend/webapp/src/pages/Profile.js
+++ frontend/webapp/src/pages/Profile.js
@@ -1,117 +1,80 @@
-import React from 'react';
-
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
-import logo from '../user-profile-modified.png';
-import Container from '@mui/material/Container';
-
-import GameItem from '../Components/GameItem';
-import postRequest from '../util/postRequest';
-
-
+import React from "react";
+import { styled } from "@mui/material/styles";
+import { Box, Paper, Grid, Container } from "@mui/material";
+import logo from "../user-profile-modified.png";
+import GameItem from "../Components/GameItem";
+import postRequest from "../util/postRequest";
 
 const Item = styled(Paper)(({ theme }) => ({
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  }));
-  
-function FormRow() {
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary
+}));
 
-    return (
-      <React.Fragment>
-        <Grid item xs={4}>
-          <GameItem 
+function FormRow() {
+  return (
+    <React.Fragment>
+      <Grid item xs={4}>
+        <GameItem
           name='game1'
-          tags={['physical', 'fun']}
+          tags={["physical", "fun"]}
           description='this is the description for the game this is the description for the game this is the description for the game
-          this is the description for the game this is the description for the game'/>
-        </Grid>
-        <Grid item xs={4}>
-            <GameItem 
-            name='game1'
-            tags={['physical', 'fun']}
-            description='this is the description for the game this is the description for the game this is the description for the game
-            this is the description for the game this is the description for the game'/>
-        </Grid>
-        <Grid item xs={4}>
-            <GameItem 
-            name='game1'
-            tags={['physical', 'fun']}
-            description='this is the description for the game this is the description for the game this is the description for the game
-            this is the description for the game this is the description for the game'/>
-        </Grid>
-      </React.Fragment>
-    );
-  }
+          this is the description for the game this is the description for the game'
+        />
+      </Grid>
+      <Grid item xs={4}>
+        <GameItem
+          name='game1'
+          tags={["physical", "fun"]}
+          description='this is the description for the game this is the description for the game this is the description for the game
+            this is the description for the game this is the description for the game'
+        />
+      </Grid>
+      <Grid item xs={4}>
+        <GameItem
+          name='game1'
+          tags={["physical", "fun"]}
+          description='this is the description for the game this is the description for the game this is the description for the game
+            this is the description for the game this is the description for the game'
+        />
+      </Grid>
+    </React.Fragment>
+  );
+}
 
 function Profile() {
+  const [gameList, setGameList] = React.useState([]);
+  React.useEffect(() => {
+    postRequest("getUserProfile", { username: "sampleUser" }, res => {
+      setGameList(res);
+      console.log(gameList);
+    });
+  }, []);
 
-    const [gameList, setGameList] = React.useState([]);
-    React.useEffect(
-        ()=>{
-            postRequest("getUserProfile", {username: "sampleUser"}, (res) => {
-                setGameList(res);
-                console.log(gameList);
-            })
-        }, []);
-    
-    return (
-
-    //    <div>
-    //     <Container maxWidth="md">
-    //         <Box sx={{ bgcolor: '#cfe8fc', height: '40vh' }}>
-
-    //             <h1 className="profile_page" className="title is-1">Profile</h1>
-    //             <img src={logo} alt="Logo" />;
-    //         </Box>
-    //     </Container>
-    // </div>
-
-        <Box sx={{ flexGrow: 1 }}>
-        <Grid container spacing={3}>
-            <Grid container item spacing={3}>
-            <FormRow />
-            </Grid>
-            <Grid container item spacing={3}>
-            <FormRow />
-            </Grid>
-        </Grid>
+  return (
+    <div>
+      <Container maxWidth='md'>
+        <Box sx={{ bgcolor: "#cfe8fc", height: "40vh" }}>
+          <h1 className='profile_page' className='title is-1'>
+            Profile
+          </h1>
+          <img src={logo} alt='Logo' />;
         </Box>
-    );
-};
+      </Container>
 
-
-// const styles = StyleSheet.create({
-//     navBar: {
-//         height: 60,
-//         flexDirection: 'row',
-//         justifyContent: 'space-between',
-//         alignItems: 'center',
-//         backgroundColor: 'blue',
-//     },
-//     leftContainer: {
-//         flex: 1,
-//         flexDirection: 'row',
-//         justifyContent: 'flex-start',
-//         backgroundColor: 'green'
-//     },
-//     rightContainer: {
-//         flex: 1,
-//         flexDirection: 'row',
-//         justifyContent: 'flex-end',
-//         alignItems: 'center',
-//         backgroundColor: 'red',
-//     },
-//     rightIcon: {
-//         height: 10,
-//         width: 10,
-//         resizeMode: 'contain',
-//         backgroundColor: 'white',
-//     }
-// });
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={3}>
+          <Grid container item spacing={3}>
+            <FormRow />
+          </Grid>
+          <Grid container item spacing={3}>
+            <FormRow />
+          </Grid>
+        </Grid>
+      </Box>
+    </div>
+  );
+}
 
 export default Profile;
